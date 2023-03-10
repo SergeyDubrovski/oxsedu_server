fde0d94e5e639f7d44bd7aaf2bde032b0a336bbb
SergeyDubrovski <sovelectron@gmail.com>
2023-03-10 15:36:28 +0300

    8st

--- controllers/deviceController.js
+++ controllers/deviceController.js
@@ -1,27 +1,46 @@
-const uuid = require ('uuid') // библиотека создания id
-const path = require ('path');
+const uuid = require('uuid') // библиотека создания id
+const path = require('path');
 const { Device } = require('../models/models');
+const ApiError = require('../error/ApiError');
 
 class deviceController {
-    async create(req, res) {
-        const { name, price, brandId, typeId } = req.body;
-        const { img } = req.files;
-        let fileName = uuid.v4 + '.jpg'; // создание уникального имени файла
-        img.mv(path.resolve(__dirname,'..', 'static', fileName)); //создание файла и запись его в директрорию static
-    const device = await Device.create({name, price, brandId, typeId, fileName})
-    
+    async create(req, res, next) {
+        try {
+            const { name, price, brandId, typeId } = req.body;
+            const { img } = req.files;
+
+            let fileName = uuid.v4() + '.jpg'; // создание уникального имени файла
+            await img.mv(path.resolve(__dirname, '..', 'static', fileName)); //создание файла и запись его в директрорию static
+            const device = await Device.create({ name, price, img: fileName, brandId, typeId })
+
+            return res.status(200).json(device);
+        } catch (error) {
+            return next(ApiError.badRequest(error.messages))
+        }
+
+
     }
     async getAll(req, res) {
-
+        const devices = await Device.findAll();
+        return res.status(200).json(devices)
     }
     async getOne(req, res) {
-
+        console.log(req.params);
+       const { id } = req.params;
+       console.log(id);
+      const devices = await Device.findOne({ id });
+        res.status(200).json(devices)
     }
 
     async delete(req, res) {
-
+        const { name } = req.body;
+        const devices = await Device.findOne({ name });
+        await devices.destroy();
+        const devices2 = await Device.findAll();
+        return res.status(200).json(devices2);
+    }
     }
 
-}
+
 
 module.exports = new deviceController();
--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const express = require('express')
 const cors = require('cors');
 const sequelize = require('./db.js')
 const fileUpload = require('express-fileupload')
+const path = require('path')
 const models = require('./models/models.js');
 const router = require('./routers/router.js');
 const errorHandlingMiddleware = require('./middleware/ErrorHandlingMiddleware.js');
@@ -18,6 +19,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(fileUpload({})); // работа с файлами в express
+app.use(express.static(path.resolve(__dirname, 'static'))); // доступ к папке sttic из браузера
 app.use('/api', router);    
 
 app.use(errorHandlingMiddleware); //middleware обработки ошибок подключается последнм в цепочке
